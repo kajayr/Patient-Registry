a19e8d07382b838cdaa3355e90feaa2ce8241762
gdin <alphadeegee@gmail.com>
2021-06-20 22:09:35 -0400

    Get mapping changed

--- src/service/patientsDataService.jsx
+++ src/service/patientsDataService.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 class PatientDataService {
     
     getAllPatients() {
-        return axios.get(`http://localhost:8080/getAllPatients`);
+        return axios.get(`http://localhost:8080/retrieveAllPatients`);
     }
     
     deletePatient(id) {
